a3487fdc20e070707b3a7a60a784595d6b928a10
Jared Jones <jkjones.dev@gmail.com>
2018-03-26 17:45:21 -0500

    Left in line blocking set

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.use(function(req, res, next) {
     if(allowedOrigins.indexOf(origin) > -1){
        res.header('Access-Control-Allow-Origin', origin);
     }
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST");
